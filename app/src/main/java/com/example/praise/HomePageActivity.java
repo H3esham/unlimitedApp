b2e238e86844e62820bdf8c5553bf45c984b07b1
lokas <lokashafeek7755@gmail.com>
2022-04-20 12:35:35 +0200

    init main layout

--- app/src/main/java/com/example/praise/HomePageActivity.java
+++ app/src/main/java/com/example/praise/HomePageActivity.java
@@ -1,10 +1,15 @@
 package com.example.praise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.praise.models.Category;
+import com.example.praise.recyclerviewlist.CategoryAdapter;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -12,6 +17,20 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Category[] categories = new Category[]{
+                new Category("Praise","Praise is a tool to convert Arabic numerals to English numerals" ,R.drawable.background_design),
+                new Category("Number Convert","Praise is a tool to convert Arabic numerals to English numerals", R.drawable.background_design),
+                new Category("Currency Convert", "Praise is a tool to convert Arabic numerals to English numerals",R.drawable.background_design),
+                new Category("Qibla","Praise is a tool to convert Arabic numerals to English numerals", R.drawable.background_design)
+        };
+
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new CategoryAdapter(categories));
+
 
     }
 
---
+++ app/src/main/java/com/example/praise/recyclerviewlist/CategoryAdapter.java
@@ -0,0 +1,50 @@
+package com.example.praise.recyclerviewlist;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.praise.R;
+import com.example.praise.models.Category;
+
+public class CategoryAdapter  extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    private Category[] categories;
+
+    public CategoryAdapter(Category[] categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Category category = categories[position];
+        holder.name.setText(category.getName());
+        holder.description.setText(category.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView description;
+        public ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.category_name);
+            description = (TextView) itemView.findViewById(R.id.category_description);
+            image = (ImageView) itemView.findViewById(R.id.category_image);
+        }
+    }
+}
