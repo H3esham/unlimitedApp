a5e814463014ae675f078c98288dcb8496b4e0a8
lokas <lokashafeek7755@gmail.com>
2022-05-06 23:39:17 +0200

    add LoadingActivity

---
+++ app/src/main/java/com/example/praise/LoadingActivity.java
@@ -0,0 +1,27 @@
+package com.example.praise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        //go to the main activity
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, HomePageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
