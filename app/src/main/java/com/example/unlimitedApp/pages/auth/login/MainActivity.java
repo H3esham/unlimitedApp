a7c76d8a3138ca8adff4e0f7466f51f9fe9ade1c
lokas <lokashafeek7755@gmail.com>
2022-05-13 13:59:19 +0200

    add base auth view (login)

---
+++ app/src/main/java/com/example/unlimitedApp/pages/auth/login/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.unlimitedApp.pages.auth.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.unlimitedApp.R;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    TextView register_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pages_login_activity);
+
+        register_btn = (TextView) findViewById(R.id.register_btn);
+        register_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register_btn:
+                Intent intent = new Intent(v.getContext(), com.example.unlimitedApp.pages.auth.register.MainActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/unlimitedApp/pages/auth/register/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.unlimitedApp.pages.auth.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.unlimitedApp.R;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pages_registre_activity);
+    }
+}
