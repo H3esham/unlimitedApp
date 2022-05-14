6c29ae77caa85323bd78aede3f2a98da238ee4cb
lokas <lokashafeek7755@gmail.com>
2022-05-14 14:55:01 +0200

    add base of vigenere

---
+++ app/src/main/java/com/example/unlimitedApp/tools/vigenereCipher/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.unlimitedApp.tools.vigenereCipher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.unlimitedApp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tools_vigenere_cipher_activity_main);
+    }
+}
